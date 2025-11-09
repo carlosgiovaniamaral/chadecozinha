d8c30be1ae2790e2b60a3d7b54feff8635d8d53f
carlosgiovaniamaral <carlos.giovaniamaral@outlook.com>
2025-11-09 13:54:31 -0300

    package correção

--- app/next.config.mjs
+++ app/next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
+    // Impede o Next de eliminar o binário do Prisma no build serverless
     serverComponentsExternalPackages: ["@prisma/client", "prisma"],
+  },
+  webpack(config) {
+    // Inclui manualmente o binário do Prisma no pacote da Vercel
+    config.externals.push("@prisma/client", "prisma");
+    return config;
   },
 };
 
